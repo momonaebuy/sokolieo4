0597c133243c603bdd312a6f5a53e33d4f04f7aa
momonaebuy <momona@angineers.com>
2019-12-29 07:14:02 +0700

    trial10

--- src/components/layout/SignedOut.js
+++ src/components/layout/SignedOut.js
@@ -8,15 +8,15 @@ const SignedOut = (props) => {
       <div>
 <nav  class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 <div class="container">
-<a class="navbar-brand" href="/">Sokoleo</a>
+<Link to='/' class="navbar-brand" >Sokoleo</Link>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 <span class="oi oi-menu"></span> Menu</button>
 <div class="collapse navbar-collapse" id="ftco-nav">
 <ul class="navbar-nav ml-auto">
-<li class="nav-item"><a href="./signin" class="nav-link"><i  class="ion-ios-log-in pr-2"/>Log In</a></li>
+<li class="nav-item"><Link to='/signin' class="nav-link"><i  class="ion-ios-log-in pr-2"/>Log In</Link></li>
 
-<li class="nav-item"><a href="./contactus" class="nav-link"><i  class="ion-ios-call pr-2"/>Contact us</a></li>
-<li class="nav-item"><a href="./aboutus" class="nav-link"><i  class="ion-ios-home pr-2"/>About Us</a></li>
+<li class="nav-item"><Link to='/contactus' class="nav-link"><i  class="ion-ios-call pr-2"/>Contact us</Link></li>
+<li class="nav-item"> <Link to='/aboutus' class="nav-link"><i  class="ion-ios-home pr-2"/>About Us</Link></li>
 </ul>
 </div>
 </div>
